125bb6de64e3a7f9c4973ede01107d2e19eb08be
Alexander Brown <web.zander@outlook.com>
2016-06-02 17:10:43 +0100

    ok so trying to use logger instead of print but getting nothing

--- tiborcim.py
+++ tiborcim.py
@@ -1,12 +1,16 @@
 from tkinter.ttk import Frame, Button, Notebook
 from tkinter.filedialog import askopenfilename
-from tkinter.messagebox import showerror, showinfo
+from tkinter.messagebox import showerror, showinfo, askokcancel
 from tkinter import Menu, DISABLED, NORMAL, END
 from tkinter.scrolledtext import ScrolledText
+
+import logging
+logging.basicConfig(filename='tiborcim.log',level=logging.INFO)
 
 class CimFilePage(Notebook):
     def __init__(self, parent):
         Notebook.__init__(self, parent)
+        logger = logging.getLogger(__name__)
         self.page_tiborcim = Frame(self)
         self.page_python = Frame(self)
         self.add(self.page_tiborcim, text='Tiborcim')
@@ -15,9 +19,11 @@ class CimFilePage(Notebook):
         self.text_tiborcim.pack(expand=1, fill="both")
         self.text_python = ScrolledText(self.page_python, state=DISABLED)
         self.text_python.pack(expand=1, fill="both")
+        self.saved = True;
 
     def save_file(self):
-        print('Save ' + self.filename)
+        self.saved = True;
+        logging.debug('Save ' + self.filename)
 
     def convert_file(self):
         from tibc import compiler as tibc
@@ -31,15 +37,17 @@ class CimFilePage(Notebook):
             self.text_python.insert(END, f.read())
             self.text_python.config(state=DISABLED)
         except:
-            print("That's Odd")
+            logging.warning("That's Odd")
 
     def save_file_as(self, name):
+        self.saved = True;
         self.filename = name
         save_file()
         
     def load_file(self, name):
+        self.saved = True;
         self.filename = name
-        print('Load ' + name)
+        logging.debug('Load ' + name)
         try:
             f = open(name)
             self.text_tiborcim.delete(1.0, END)
@@ -53,6 +61,18 @@ class CimFilePage(Notebook):
 
     def view_python(self):
         self.select(self.page_python)
+
+    def close(self):
+        if not self.saved:
+            if askokcancel("Unsaved Changes", "Somefiles havent been saved!"):
+                logging.debug("Close Anyway")
+                return True
+            else:
+                logging.debug("Cancel")
+                return False
+        else:
+            return True
+
         
 class CimApp(Frame):
     def __init__(self):
@@ -62,11 +82,15 @@ class CimApp(Frame):
         self.master.iconbitmap('icon.ico')
         self.files = []
         self.pack(expand=1, fill="both")
+        self.master.minsize(300,300)
+        self.master.geometry("500x500")
 
         menubar = Menu(self.master)
         self.fileMenu = Menu(self.master, tearoff=0)
         self.fileMenu.add_command(label="Open...", command=self.load_file,
                                   underline=1, accelerator="Ctrl+O")
+        self.fileMenu.add_command(label="Close", command=self.close_file,
+                                  underline=1, accelerator="Ctrl+W")
         self.fileMenu.add_separator()
         self.fileMenu.add_command(label="Exit", command=self.file_quit, underline=1)
         menubar.add_cascade(label="File", menu=self.fileMenu, underline=1)
@@ -88,9 +112,11 @@ class CimApp(Frame):
                                        variable=viewmode, value="python")
         menubar.add_cascade(label="View", menu=self.menu_view, underline=1)
 
-        self.bind_all("<Control-o>", self.load_file_keyb)
-        self.bind_all("<Control-t>", self.convert_file_keyb)
-        self.bind_all("<Control-b>", self.flash_file_keyb)
+        self.bind_all("<Control-o>", self.load_file)
+        self.bind_all("<Control-t>", self.convert_file)
+        self.bind_all("<Control-b>", self.flash_file)
+        self.bind_all("<Control-w>", self.close_file)
+        self.master.protocol("WM_DELETE_WINDOW", self.file_quit)
 
         self.file_tabs = Notebook(self)
         self.nb = CimFilePage(self.file_tabs)
@@ -111,24 +137,19 @@ class CimApp(Frame):
     def view_python(self):
         self.current_file().view_python()
 
-    def load_file(self):
+    def load_file(self, event=None):
         fname = askopenfilename(filetypes=(("Tiborcim", "*.tibas"),("All files", "*.*") ))
         if fname:
             self.add_file(fname)
 
-    def load_file_keyb(self, event):
-        self.load_file()
-
-    def convert_file(self):
+    def convert_file(self, event=None):
         self.current_file().convert_file()
 
-    def convert_file_keyb(self, event):
-        self.convert_file()
-
     def current_file(self):
+        logging.debug(int(self.file_tabs.index(self.file_tabs.select())))
         return self.files[int(self.file_tabs.index(self.file_tabs.select()))]
 
-    def flash_file(self):
+    def flash_file(self, event=None):
         from tibc import flash
         from tibc import TibcStatus as status
         self.current_file().convert_file()
@@ -138,10 +159,18 @@ class CimApp(Frame):
         else:
             showerror(title='Failure', message='An Error Occured. Code: %s' % result, parent=self.master)
 
-    def flash_file_keyb(self, event):
-        self.flash_file()
+    def close_file(self, event=None):
+        logging.debug("Close File")
+        if self.current_file().close():
+            self.file_tabs.forget(self.current_file())
+            self.files.remove(self.current_file)
+        
 
     def file_quit(self):
+        for ndx, member in enumerate(self.files):
+            logging.debug(self.files[ndx].saved)
+            self.files[ndx].close()
+
         self.quit()
 
 _HELP_TEXT = """
@@ -158,4 +187,4 @@ if __name__ == "__main__":
         args = parser.parse_args(argv)
         CimApp().mainloop()
     except Exception as ex:
-        print(ex)
+        logging.debug(ex)
